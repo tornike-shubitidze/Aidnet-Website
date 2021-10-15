7778afeb61f7a878ce5826c25162c73905821008
tornike-shubitidze <shubavar@gmail.com>
2021-10-15 14:12:08 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let searchBtn = document.querySelector(".search-button");
+
+searchBtn.addEventListener('click', function () {
+    let searchBtnIcon = document.querySelector(".search-button i");
+    if (searchBtnIcon.classList.contains("fa-search")) {
+        searchBtnIcon.classList.remove("fa-search");
+        searchBtnIcon.classList.add("fa-times")
+    } else {
+        searchBtnIcon.classList.remove("fa-times");
+        searchBtnIcon.classList.add("fa-search")
+    };
+
+})
+
+
+function initMap() {
+    const myLatLng = { lat: 41.673409, lng: 44.832981 };
+    const map = new google.maps.Map(document.querySelector(".map"), {
+        zoom: 19,
+        center: myLatLng,
+    });
+
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Hello World!",
+    });
+}
+
+window.onscroll = function () {
+    if (this.oldScroll > this.scrollY) {
+        document.getElementById('up-arrow').style.visibility = 'hidden';
+    }
+    else if (this.oldScroll < this.scrollY) {
+        document.getElementById('up-arrow').style.visibility = 'visible';
+    }
+    this.oldScroll = this.scrollY;
+}
+
+
+
+
